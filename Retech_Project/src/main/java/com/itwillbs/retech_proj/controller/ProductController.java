c65aa475932c89d3f4bc080978d006b9fc8c67dc
user <user@DESKTOP-TTHBAV5>
2024-08-09 13:57:49 +0900

    0809 진성민

--- Retech_Project/src/main/java/com/itwillbs/retech_proj/controller/ProductController.java
+++ Retech_Project/src/main/java/com/itwillbs/retech_proj/controller/ProductController.java
@@ -1,13 +1,12 @@
 package com.itwillbs.retech_proj.controller;
 
-import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import com.itwillbs.retech_proj.service.AdminService;
+
+
 
 
 @Controller
