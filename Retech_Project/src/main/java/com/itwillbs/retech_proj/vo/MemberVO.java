588b6e23fed50e7b7b5d00064bbb2715aeaa76db
a4090 <a4090@DESKTOP-DTBN5KT>
2024-09-08 20:55:33 +0900

    수안 이것저것

--- Retech_Project/src/main/java/com/itwillbs/retech_proj/vo/MemberVO.java
+++ Retech_Project/src/main/java/com/itwillbs/retech_proj/vo/MemberVO.java
@@ -8,21 +8,19 @@ import lombok.Data;
 
 @Data
 public class MemberVO {
-	private String member_id;
-	private String member_nickname;
-	private String member_passwd;
-	private String member_name; 
-	private Date member_birth; 
-	private String member_phone; 
-	private String member_status; 
-	private String member_mail_auth; 
-	private String member_postcode; 
-	private String member_address1; 
-	private String member_address2;  
-	private String member_profile; 
-	private int member_isAdmin;
-	private Date member_motivation; 
-	private MultipartFile profile; 
+    private String member_id;
+    private String member_nickname;
+    private String member_passwd;
+    private String member_name; 
+    private Date member_birth; 
+    private String member_phone; 
+    private String member_status; 
+    private String member_mail_auth; 
+    private String member_postcode; 
+    private String member_address1; 
+    private String member_address2;  
+    private String member_profile; // 파일 경로를 저장하는 필드
+    private int member_isAdmin;
+    private Date member_motivation; 
+    private MultipartFile profile; // 업로드된 파일을 저장하는 필드
 }
-
-
